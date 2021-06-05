e36092b3434872a7960990aadfb50f6ef86b476b
Joy Kirii <joykirii@gmail.com>
2021-06-05 13:16:32 +0300

    create user service

---
+++ src/app/services/users/user.service.ts
@@ -0,0 +1,18 @@
+import { Users } from './../../interfaces/users';
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+
+  url = 'http://127.0.0.1:8000/api/neighbourhood/'
+  constructor(private http: HttpClient) { }
+
+  fetchUsersApi(): Observable<Users[]> {
+    return this.http.get<Users[]>(this.url);
+
+  }
+}
