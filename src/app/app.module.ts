d2fd0d588887bc4f7cca6f787f1b1a4059aaf148
Joy Kirii <joykirii@gmail.com>
2021-06-05 11:42:50 +0300

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NeighbourhoodComponent } from './component/neighbourhood/neighbourhood.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NeighbourhoodComponent
   ],
   imports: [
     BrowserModule
