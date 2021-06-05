b7cd357795b66c13e5759102c48999b978d1da1e
Joy Kirii <joykirii@gmail.com>
2021-06-05 12:45:38 +0300

    create business service

--- src/app/interfaces/business.ts
+++ src/app/interfaces/business.ts
@@ -1,6 +1,7 @@
-export interface Interfaces {
+export interface Business {
   business_name:string
   admin:any
   neighbourhood:string
   business_email:string
 }
+
--- src/app/services/bizz/business.service.ts
+++ src/app/services/bizz/business.service.ts
@@ -1,9 +1,17 @@
+import { Business } from './../../interfaces/business';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 import { Injectable } from '@angular/core';
 
 @Injectable({
   providedIn: 'root'
 })
 export class BusinessService {
+  url = 'http://127.0.0.1:8000/api/business/'
+  constructor(private http: HttpClient) { }
 
-  constructor() { }
+  fetchBusinessApi(): Observable<Business[]> {
+    return this.http.get<Business[]>(this.url);
+
+  }
 }
